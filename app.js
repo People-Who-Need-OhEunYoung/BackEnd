b98a1868496c2b91c7ea78f7991bb5a3900554b4
Yangsunkue <ysk9526@gmail.com>
2024-08-09 23:29:53 +0900

    feat: JWT 토큰 검증 중복 코드 미들웨어화, 쓰이지 않는 코드 주석 처리 및 코드 정리 리팩토링

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+// const { exec } = require('child_process');  // app.js가 있는 경로를 기준으로 실행된다
 
 // 실시간 협업 에디터
 const WebSocket = require('ws');
@@ -8,13 +12,6 @@ const http = require('http');
 const dotenv = require('dotenv');
 dotenv.config();
 const { setupWSConnection } = require('y-websocket/bin/utils');
-
-const app = express();
-const bodyParser = require('body-parser');
-const { exec } = require('child_process');  // app.js가 있는 경로를 기준으로 실행된다
-const path = require('path');
-
-// 실시간 협업 에디터
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
@@ -22,10 +19,11 @@ const wss = new WebSocket.Server({ server });
 const mainPage = require('./lib/mainPage');
 const startPage = require('./lib/startPage');
 const myPage = require('./lib/myPage');
-const codeReview = require('./lib/codeReview');
 const codeEditor = require('./lib/codeEditor');
 const problemList = require('./lib/problemList');
-const ai = require('./lib/ai');
+const authenticateToken = require('./lib/auth');
+// const codeReview = require('./lib/codeReview');
+// const ai = require('./lib/ai');
 
 wss.on('connection', (ws, req) => {
     setupWSConnection(ws, req);
@@ -75,137 +73,85 @@ app.post('/tokenTest', (req, res) => {
 });
 
 // 1. 헤더 데이터 ( 닉네임, 크레딧, 현재 포켓몬ID 뿌려주기 ) => 완성
-app.post('/headerData', (req, res) => {
+app.post('/headerData', authenticateToken, (req, res) => {
     mainPage.headerData(req, res);
 });
 
-// 27. 메인페이지 헤더 데이터 ( 1번 헤더 데이터 + 포켓몬레벨 + 경험치 ) => 완성
-app.post('/mainPageHeaderData', (req, res) => {
-    mainPage.mainPageHeaderData(req, res);
-});
-
 // 2. 똥 치우기 버튼 => 완성
-app.post('/clearPoo', (req, res) => {
+app.post('/clearPoo', authenticateToken, (req, res) => {
     mainPage.clearPoo(req, res);
 });
 
 // 25. 똥 개수 가져오기 => 완성
-app.post('/getPoo', (req, res) => {
+app.post('/getPoo', authenticateToken, (req, res) => {
     mainPage.getPoo(req, res);
 });
 
 // 6. 도감 출력 => 완성
-app.get('/book', (req, res) => {
+app.get('/book', authenticateToken, (req, res) => {
     mainPage.book(req, res);
 });
 
-
 // 9. 포켓몬 id 받아서 book 등록 후 현재 포켓몬 설정 => 완성
-app.post('/evolution', (req, res) => {
+app.post('/evolution', authenticateToken, (req, res) => {
     mainPage.evolution(req, res);
 });
 
-
-// 37. 전설의 포켓몬 뽑기 => 작업 진행 중
-app.post('/legendGambling', (req, res) => {
+// 37. 전설의 포켓몬 뽑기 => 완성
+app.post('/legendGambling', authenticateToken, (req, res) => {
     mainPage.legendGambling(req, res);
 });
 
-
 // 5. 마이페이지 입장 시 데이터 뿌려주기 => 완성
-app.post('/myPage', (req, res) => {
+app.post('/myPage', authenticateToken, (req, res) => {
     myPage.myPage(req, res);
 });       
 
 // 7. 현재 포켓몬 변경 => 완성
-app.post('/changeMonster', (req, res) => {
+app.post('/changeMonster', authenticateToken, (req, res) => {
     myPage.changeMonster(req, res);
 });
 
 // 8. 닉네임 수정 => 완성
-app.post('/changeNickName', (req, res) => {
+app.post('/changeNickName', authenticateToken, (req, res) => {
     myPage.changeNickName(req, res);
 });
 
 // 32. 사용자가 푼 문제 + 획득 크레딧/경험치 및 기타정보 주기 => 완성
-app.get('/resolvedProblems', (req, res) => {
+app.get('/resolvedProblems', authenticateToken, (req, res) => {
     myPage.resolvedProblems(req, res);
 })
 
 // 31. 문제 출력
-app.get('/problemList', (req, res) => {
+app.get('/problemList', authenticateToken, (req, res) => {
     problemList.problemList(req, res);
 });
 
-// 11. 코드리뷰 로비 입장 시 방목록 뿌려주기
-// app.get('/reviewList', (req, res) => {
-//     codeReview.reviewList(req, res);
-// });
-
-app.get('/reviewList', (req, res) => {
-    codeReview.reviewList(req, res);
-});
-
-// 13. 방 만들기 => 완성
-app.post('/createReview', (req, res) => {
-    codeReview.createReview(req, res);
-});
-
-// 35. 해결 버튼 => 완성
-app.post('/complete', (req, res) => {
-    codeReview.complete(req, res);
-})
-
-// 36. 강퇴 버튼
-app.post('/kick', (req, res) => {
-    codeReview.kick(req, res);
-})
-
-
-// 30. 채점 가능한 문제 보기 => 수정필요
-app.post('/viewProblem', (req, res) => {
-    codeEditor.viewProblem(req, res);
-});
-
-// 33. 문제 에디터 입장할 때 resolved 테이블에 데이터 추가
-app.post('/selectOrUpdateResolvedProblem', (req, res) => {
-    codeEditor.selectOrUpdateResolvedProblem(req, res);
-})
-
 // 17.1 코드 채점 => 완성
-app.post('/runCode', (req, res) => {
+app.post('/runCode', authenticateToken, (req, res) => {
     codeEditor.runCode(req, res);
 });
 
+// 33. 문제 에디터 입장할 때 resolved 테이블에 데이터 추가
+app.post('/selectOrUpdateResolvedProblem', authenticateToken, (req, res) => {
+    codeEditor.selectOrUpdateResolvedProblem(req, res);
+})
+
 // 34. 경과시간, 제한시간 저장하는 API
-app.post('/setTime', (req, res) => {
+app.post('/setTime', authenticateToken, (req, res) => {
     codeEditor.setTime(req, res);
 });
 
-// AI 수정사항 : 토큰 만료 시 질문할 수 없도록 수정해야 함
-// 27. AI - 알고리즘 힌트 => 완성
-app.post('/aiAlgoHint', (req, res) => {
-    ai.aiAlgoHint(req, res);
-});
 
-// 28. AI - 알고리즘 정답/오답 코드 피드백 => 완성
-app.post('/aiAlgoFeedBack', (req, res) => {
-    ai.aiAlgoFeedBack(req, res);
-});
 
-// 29. AI - 일반 코드 피드백 => 완성
-app.post('/aiFeedBack', (req, res) => {
-    ai.aiFeedBack(req, res);
-
-});
 
 // //서버 실행
-// server.listen(3000 || 44444, () => {
-//     console.log(`running on port:3000`);
-// });
+server.listen(3000 || 44444, () => {
+    console.log(`running on port:3000`);
+});
 
 // develop에 올릴 때 process.env.PORT로 바꾸기
-server.listen(process.env.PORT || 44444, () => {
-    console.log(process.env.PORT);
-    console.log(`Server running on port:${process.env.PROFILE}`);
-});
+// server.listen(process.env.PORT || 44444, () => {
+//     console.log(process.env.PORT);
+//     console.log(`Server running on port:${process.env.PROFILE}`);
+// });
